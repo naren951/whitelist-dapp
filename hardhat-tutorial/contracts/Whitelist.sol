c413bfcbe6b4ba534a0aa5fea7e8632b31406e08
Narendiran Arthanarieswaran <narendiran.work@gmail.com>
2022-08-03 12:14:04 +0530

    initial commit

---
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+
+    uint8 public maxWhitelistedAddresses;
+
+    uint8 public numAddressesWhitelisted;
+
+    mapping(address => bool) public whitelistedAddresses;
+
+    constructor(uint8 _maxWhitelistedAddresses){
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        require(!whitelistedAddresses[msg.sender],"Sender already in whitelist");
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "Max limit reached");
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted+=1;
+    }
+}
